6ad5900e3c7624c919a77f93d59f4696c8f3b0d4
Phan Trung Kien <kienpt17@viettel.com.vn>
2020-03-23 16:37:23 +0700

    Add VAD by Python

--- stream_audio/VAD.py
+++ stream_audio/VAD.py
@@ -5,22 +5,36 @@ from scipy.io import wavfile
 
 
 
-WAVE_INPUT_FILENAME = "../data/speaker0250-0020.wav"
-fs, audio = wavfile.read(WAVE_INPUT_FILENAME)
-print(audio.shape)
+input_file = "../data/speaker0250-0020.wav"
+fs, audio = wavfile.read(input_file)
+num_samples, num_channels = audio.shape
 
+win_size = 0.01
+samples_per_frame = int(fs*win_size)
 
+# init VAD
 vad = webrtcvad.Vad()
 vad.set_mode(1)
 
-begin = 0
-sample_per_frame = int(RATE*10/1000)
 
-while True:
-    end = begin + sample_per_frame
-    if end > result.shape[0]:
-        break
-    test_frame = (result[begin:end,0]).tobytes()
-    print(vad.is_speech(test_frame, RATE))
-    begin = begin + sample_per_frame
+# calculate VAD
+result = np.zeros((num_samples,num_channels), dtype='int16')
+for channel in range(num_channels):
+    begin = 0
+    print("Channel", channel)
+    while begin < num_samples:
+        end = min(begin + samples_per_frame, num_samples)
+        frame = audio[begin:end, channel]
+        if frame.shape[0] < samples_per_frame:
+            remain_len = samples_per_frame-frame.shape[0]
+            frame = np.concatenate((frame, np.zeros(remain_len, dtype='int16')), axis=0)
+        
+        frame = frame.tobytes()
+        t = vad.is_speech(frame, fs)
+       
+        result[begin:end, channel].fill(int(t)) 
+        begin = begin + samples_per_frame
+
+np.savetxt('VAD.txt',  np.transpose(result), fmt= '%d')
+        
 
